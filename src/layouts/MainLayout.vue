cfa1982256b197b5027e6e5bb35b8ad8ea2a5486
sammy-code98 <sammysmart98@yahoo.com>
2022-06-15 21:43:10 +0100

    add left drawer

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -8,8 +8,25 @@
       </q-toolbar>
     </q-header>
 
-    <q-drawer show-if-above v-model="leftDrawerOpen" side="left" bordered>
-      <q-icon name="font_download" />
+    <q-drawer show-if-above v-model="leftDrawerOpen" side="left" bordered :width="283">
+      <q-icon name="fas fa-dove" size="lg" color="primary" class="q-pa-md" />
+
+      <q-list>
+        <q-item clickable v-ripple to="/">
+          <q-item-section avatar>
+            <q-icon  name="home" size="md" />
+          </q-item-section>
+
+          <q-item-section class="text-h6 text-weight-bold">Home</q-item-section>
+        </q-item>
+         <q-item clickable v-ripple to="/about">
+          <q-item-section avatar>
+            <q-icon  name="help" size="md" />
+          </q-item-section>
+
+          <q-item-section class="text-h6 text-weight-bold">About</q-item-section>
+        </q-item>
+      </q-list>
     </q-drawer>
 
     <q-drawer show-if-above v-model="rightDrawerOpen" side="right" bordered>
