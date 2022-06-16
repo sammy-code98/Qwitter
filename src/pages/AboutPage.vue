acf1c703d4a254188333226b3091d4ffb1a0b51d
sammy-code98 <sammysmart98@yahoo.com>
2022-06-16 21:00:09 +0100

    feat:  add about page mockup

--- src/pages/AboutPage.vue
+++ src/pages/AboutPage.vue
@@ -1,13 +1,27 @@
 <template>
-  <q-page>
-  <h5>about</h5>
+  <q-page class="q-pa-lg">
+    <h4 class="q-mt-none q-mb-md text-weight-bold">About Qwitter</h4>
+    <div class="text-body1">
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repudiandae
+        corrupti necessitatibus sequi earum qui fuga fugit laudantium sed
+        nostrum perferendis rerum aut ut, quisquam, magni pariatur nemo eveniet,
+        quis officiis.
+      </p>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repudiandae
+        corrupti necessitatibus sequi earum qui fuga fugit laudantium sed
+        nostrum perferendis rerum aut ut, quisquam, magni pariatur nemo eveniet,
+        quis officiis.
+      </p>
+    </div>
   </q-page>
 </template>
 
 <script>
-import { defineComponent } from 'vue';
+import { defineComponent } from "vue";
 
 export default defineComponent({
-  name: 'AboutPage'
-})
+  name: "AboutPage",
+});
 </script>
